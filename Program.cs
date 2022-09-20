b49ed6b94c32356afe814e6e48b4bd1957bbd833
birnazarova <birnazarova@gmail.com>
2022-09-19 22:08:38 -0700

    the program code

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿
-string[] stringArray = ["hello", "2", "world", ":-)"];
+﻿string[] stringArray = new string[]{"hello", "2", "world", ":-)"};
+
+var resultArray = Array.Empty<string>();
+int i = 0;
+
+while (i < stringArray.Length)
+{
+    if (stringArray[i].Length <= 3)
+    {
+        string element = stringArray[i];
+        resultArray = resultArray.Append(element).ToArray();
+    }
+    i++;
+}
+
+foreach (string item in resultArray){
+    Console.Write(item);
+    Console.Write(" ");
+} 
+Console.WriteLine();
